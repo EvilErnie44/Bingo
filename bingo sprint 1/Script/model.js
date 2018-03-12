297003ec9cbae1ecdfd3953fe9f5e6b020c97abc
EvilErnie44 <33355726+EvilErnie44@users.noreply.github.com>
2018-03-12 12:30:17 +0000

    Add files via upload

--- bingo sprint 1/Script/model.js
+++ bingo sprint 1/Script/model.js
@@ -3,29 +3,23 @@
 // JavaScript source code
 var cellNumObj = [];
 var cardNum = [];
+
 function newNum() {
 return Math.floor((Math.random() * 76) + 1);
 }
 
-//Controller - controls the use of the events.
-window.onload = function cellRanNum() {
-cellNum();
-genRandNum();
-bingoCard1();
-//bingoCard2();
-}
-
-//create awway with square strings
+/*create array with square strings
 function cellNum() {
  for (i=0; i<24; i++){
   cellNumObj.push("square"+(i));
  }
 }
+*/
 
 //create array with random numbers - no dupes.
-function genRandNum() {
-  //loop needs changing into function. 
-for (i=0; i<24;) {
+function genRandNum() { }
+/*  //loop needs changing into function.
+for (var i=0; i<24;) {
 //This makes a call to generate a random number with each iteration of the loop.
   newNum();
 //loop cont
@@ -38,21 +32,13 @@ if (cardNum.indexOf(newNum)!== -1) {continue;
   }
 }
 
-//Put any ran num into the bingo squares
-function bingoCard1() {
-for (var i=0; i<24; i++) {
-document.getElementById("square"+i).innerHTML = newNum();
-}
-}
-
-//creates array of random numbers with duplicates
+/*creates array of random numbers with duplicates
 function genRandNum() {
 for (i=0; i<24; i++) {
 cardNum.push(Math.floor((Math.random() * 76) + 1));
 }
-
 }
-/*
+
 New Change
 window.onload=bingoCard;
 
